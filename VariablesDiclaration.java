20d8c464f3e21b5ce6ac0a17361ffcf271ac65fc
Johnathan joshua <116352769+Johanthan0107@users.noreply.github.com>
2022-11-17 18:17:56 +0530

    Add files via upload

---
+++ VariablesDiclaration.java
@@ -0,0 +1,8 @@
+public class Variables {
+public static void main (String [] args){
+ String name="Johnathan";//String:- it's stores text,Such as "Hello"or"Johnathan"String value surrounded by dibble quotes.
+ int myNumber=939060058;//int:- its stores integers (Whole number), without decimal,Such as 123456... or -123456....
+System.out.println(name);
+System.out.println(myNumber);
+ }
+}
