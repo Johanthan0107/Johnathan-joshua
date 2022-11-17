13a103e996e309d262520c3eded68eac7515869c
Johnathan joshua <116352769+Johanthan0107@users.noreply.github.com>
2022-11-17 18:06:56 +0530

    Add files via upload

---
+++ Identifier.java
@@ -0,0 +1,14 @@
+//All Java variables must be identified with unique names,Thae unique namea are called Identifier.
+public class Identifier {
+public static void main (String [] args){
+
+//Good
+int Minutesper_Hour=60;
+//Ok, but not so easy to understand what M actually is
+int M=60;
+System.out.println(Minutesper_Hour);//60
+System.out.println(M);//60
+ }
+}
+// we can't give variables name wit starting (lowercase letters) in middle (no spaces)
+//will give Minutesper_Hour or MinutesperHour
