2b320749b9adf77af0711b861dc9867cf7288cbf
Johnathan joshua <116352769+Johanthan0107@users.noreply.github.com>
2022-11-17 18:29:59 +0530

    Add files via upload

---
+++ VariablesMultiples.java
@@ -0,0 +1,18 @@
+public class Multiple_variables{
+	
+public static void main (String[] args){
+	
+	String statment="The total x,y,z Amount is";
+int x=5,y=10,z=15;
+/*OR int x=5;
+     int y=10;
+     int z=15;
+     
+  OR
+     int x,y,z;
+     x=y=z=50;*/
+System.out.println("The total x,y,z Amount is");
+System.out.println(x+y+z);
+
+ }
+}
