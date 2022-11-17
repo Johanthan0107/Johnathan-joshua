25a16b1de0c58e56be0693e95889eef7ba46c4c1
Johnathan joshua <116352769+Johanthan0107@users.noreply.github.com>
2022-11-17 19:13:31 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,21 @@
+//A variable in java must be a Specified datatype.
+class Datatype
+{
+	public static void main (String[] args)
+	{
+		int myNumber=260197;
+		float myfloatNumber=17.98f;
+		char myletter='J';
+boolean myBool=true;
+String myText="Think Positive";
+ double myWight=60;
+
+		System.out.println(myNumber);
+		System.out.println(myfloatNumber);
+		System.out.println(myletter);
+		System.out.println(myBool);
+		System.out.println(myText);
+		System.out.println(myWight);
+	
+	}
+}
