bf385d5f2aab3fdd2754c62d508f8081db51a951
Johnathan joshua <116352769+Johanthan0107@users.noreply.github.com>
2022-11-17 18:13:30 +0530

    Add files via upload

---
+++ variablesPrint.java
@@ -0,0 +1,20 @@
+
+public class printVariable
+{
+	public static void main (String[] args)
+	{
+		String firstName="Johnathan";
+		String secondName="Joshua";
+		String fullName=firstName+secondName;
+		/*int X=10;
+		int Y=20;
+		int Z=30;*///60
+			
+		//int x=5,y=6,z=10;//21
+		
+		int x,y,z;
+		x=y=z=50;//150
+		System.out.println(fullName);
+		System.out.println(x+y+z);
+	}
+}
