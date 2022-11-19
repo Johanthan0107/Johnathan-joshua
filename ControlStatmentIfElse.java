389fc5154008a1a47bb5b88c353c8e4591a6217f
Johnathan joshua <116352769+Johanthan0107@users.noreply.github.com>
2022-11-19 18:52:11 +0530

    Add files via upload

---
+++ ControlStatmentIfElse.java
@@ -0,0 +1,13 @@
+public class If{
+public static void main (String[] args){
+int x=10;
+int y=20;
+if(x>y){
+//(x<0)or(x==y) will give any type, our wish.
+	System.out.println("x is less than than y");//x is greater than y
+}
+else{
+	System.out.println("x is greater than y");
+   }
+  }
+ }
