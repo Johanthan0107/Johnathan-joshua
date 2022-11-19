d3d6cd798d6235ec57557021464f79d75b084c98
Johnathan joshua <116352769+Johanthan0107@users.noreply.github.com>
2022-11-19 19:47:13 +0530

    Add files via upload

---
+++ ControlStatementelseif.java
@@ -0,0 +1,16 @@
+//else_if:- Block of code to be excuted if the condition1 is false and condition2 is false.
+public class else_if
+{
+	public static void main (String[] args){
+		int time =24;
+		if(time==10){
+		System.out.println("Good morning..");
+		}
+		else if (time<20){
+			System.out.println("Good day..");
+			}
+			else{
+				System.out.println("Good evening..");//Good evening..
+		}
+	}
+}
