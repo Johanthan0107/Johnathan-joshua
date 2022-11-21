7d7360e3c1cdff0f8ec2177a852e3b59f448ba05
Johnathan joshua <116352769+Johanthan0107@users.noreply.github.com>
2022-11-21 19:22:15 +0530

    Add files via upload

---
+++ ControlstatementsSwitch.java
@@ -0,0 +1,34 @@
+//Switch:- it's select one of many code blocks to be excuted.
+
+public class Main {
+  public static void main(String[] args) {
+  	
+    int day = 8;
+  
+    switch (day) {
+      case 1:
+        System.out.println("Monday");
+        break;
+      case 2:
+        System.out.println("Tuesday");
+        break;
+      case 3:
+        System.out.println("Wednesday");
+        break;
+      case 4:
+        System.out.println("Thursday");
+        break;
+      case 5:
+        System.out.println("Friday");
+        break;
+        case 6:
+        System.out.println("Saturday");
+        break;
+        case 7:
+        System.out.println("Sunday");
+        break;
+        case 8:
+        System.out.println("Saturday and Sunday:-Yahh, it's weak end");
+  }
+ }
+}
