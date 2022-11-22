b1187f835ef57486c4b17017c22d9234944ce04d
Johnathan joshua <116352769+Johanthan0107@users.noreply.github.com>
2022-11-22 21:16:36 +0530

    Update and rename ControlStatmentNestedloop.java to ControlStatment_Nested loop.java

--- ControlStatment_Nested loop.java
+++ ControlStatment_Nested loop.java
@@ -12,7 +12,9 @@ public class Main {
    // Inner loop:The "inner loop" will be executed one time for each iteration of the "outer loop":
    //This is inner loop:
       for (int j = 1; j <= 4; j++) {
-        System.out.println(" Inner: " + j); // Executes 12 times (3 * 4),How it's displayed:- out put will be start out put 1,out put2,out put3,like this, and input will be start input 1,input 2,input 3,input 4 like this.
+        System.out.println(" Inner: " + j); // Executes 12 times (3 * 4)
+/*How it's displayed:- out put will be start out put 1,out put2,out put3,like this
+ and input will be start input 1,input 2,input 3,input 4 like this.*/
 
       }
     } 
