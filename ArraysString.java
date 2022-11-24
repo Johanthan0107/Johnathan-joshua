f77a47b304fb2d222105ef67516e7d61e6848a78
Johnathan joshua <116352769+Johanthan0107@users.noreply.github.com>
2022-11-24 21:11:44 +0530

    Add files via upload

---
+++ ArraysString.java
@@ -0,0 +1,14 @@
+//Arrays:- Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
+//We are  declare an array, define the variable type with square brackets[]:
+
+public class Array{
+public static void main (String[] args){
+	
+String[] cars={"BMW","ODI","VOLVO","FORD"};
+int[] carsNumbers={101,202,303,404,505};//404
+
+System.out.println(carsNumbers[3]);
+System.out.println(cars[3]);//FORD
+ }
+}
+//NOTE:- Arrays indexs starts with0:[0] is the first element.[1] is the second elements,etc
